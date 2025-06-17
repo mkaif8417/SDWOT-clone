3c38a7c640cc362cb59544cf53881175488a1c72
mkaif8417 <mkaif6937@gmail.com>
2025-06-17 09:44:17 +0530

    Editing some error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -12,16 +13,19 @@ import Jobs from './pages/Jobs';
 function App() {
   return (
     <>
-
+   <BrowserRouter basename="/SDWOT-clone">
       <Routes>
+     
+
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/privacy" element={<Privacy />}/>
         <Route path="/Jobs" element={<Jobs />} />
         <Route path="/signin" element={<Signin />} />
+        
       </Routes>
-      
+      </BrowserRouter>
     </>
   );
 }
