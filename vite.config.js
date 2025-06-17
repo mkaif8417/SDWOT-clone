241f410c9e3b46250509c0093a38c3daa0d97807
mkaif8417 <mkaif6937@gmail.com>
2025-06-17 17:13:38 +0530

    Fixing another bug

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/sdwot-clone/',  // IMPORTANT: this should match your repo name exactly
+export default defineConfig({  
   plugins: [react()],
-  base: '/SDWOT-clone',  // IMPORTANT: this should match your repo name exactly
+  base: '/SDWOT-clone/',  // IMPORTANT: this should match your repo name exactly
 })
 
