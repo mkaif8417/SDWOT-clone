d99e795d8a7ed9edf044e0503b795f78be5fe9bd
mkaif8417 <mkaif6937@gmail.com>
2025-06-17 15:39:27 +0530

    Set base URL Github pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/SDWOT-clone/', // 👈 Must match your GitHub repo name
+  base: '/sdwot-clone/',  // IMPORTANT: this should match your repo name exactly
   plugins: [react()],
 })
