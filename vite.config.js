37a813ac05a5a744b44aa1385a0999f19bc140b8
mkaif8417 <mkaif6937@gmail.com>
2025-06-17 09:21:07 +0530

    Adding some files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/SDWOT-clone/', // 👈 Must match your GitHub repo name
   plugins: [react()],
 })
